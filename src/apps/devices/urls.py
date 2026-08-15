653caa97ad88280fd2286b597a73ca24de91e429
Rombica <magitovich@yandex.ru>
2026-08-16 01:28:04 +0500

    create device models lists

--- src/apps/devices/urls.py
+++ src/apps/devices/urls.py
@@ -5,4 +5,6 @@ app_name = 'devices'
 
 urlpatterns = [
     path('', views.device_list, name='device-list'),
+    path('models/', views.model_list, name='model-list'),
+    path('vendors/', views.vendor_list, name='vendor-list'),
 ]
--- src/apps/devices/views.py
+++ src/apps/devices/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Device
+from .models import Device, DeviceModel, Vendor
 
 
 def device_list(request):
@@ -8,3 +8,14 @@ def device_list(request):
     context = {'devices': devices}
     return render(request, 'devices/device_list.html', context)
 
+
+def model_list(request):
+    device_models = DeviceModel.objects.all()
+    context = {'device_models': device_models}
+    return render(request, 'devices/device_models_list.html', context)
+
+
+def vendor_list(request):
+    vendors = Vendor.objects.all()
+    context = {'vendors': vendors}
+    return render(request, 'devices/vendor_list.html', context)
